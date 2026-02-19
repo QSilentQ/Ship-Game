0df5e9a6e3b72281d7e49a02fc6ec01fa7d6b768
Silent <qqsilentqq@gmail.com>
2026-02-19 18:00:02 +0300

    style: added some beauty

--- Program.cs
+++ Program.cs
@@ -1,11 +1,39 @@
-﻿namespace Ships
+﻿using Ships.Entities;
+
+namespace Ships
 {
     internal class ShipGame
     {
         static void Main(string[] args)
         {
+            string? userChoise;
+
             // Это главный файл для запуска игры
-            Console.WriteLine("Welcome to the ShipGame!");
+            Console.WriteLine("          Добро пожаловать в игру!");
+            Console.WriteLine("               Морские войны\n");
+            Console.WriteLine("                   Меню\n");
+
+            Console.WriteLine("                1. Играть\n");
+            Console.WriteLine("                2. Выйти\n");
+
+            Console.Write("            Выберите действие: ");
+            while (true)
+            {
+                userChoise = Console.ReadLine()?.Trim();
+
+                if (userChoise == "1" || userChoise == "2") break;
+                Console.Write("  Неверный ввод. Пожалуйста, выберите 1 или 2: ");
+            }
+
+            switch (userChoise)
+            {
+                case "1":
+                    Console.WriteLine("             Вы выбрали: Играть");
+                    break;
+                case "2":
+                    Console.WriteLine("             Вы выбрали: Выйти");
+                    break;
+            }
         }
     }
 }
